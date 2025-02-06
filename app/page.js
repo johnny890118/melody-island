2ce0d7a5146aac17fa3fb8e090cfa0363302a121
Johnny <johnny0929560027@gmail.com>
2025-02-06 23:12:38 +0800

    refactor：rwd test

--- app/page.js
+++ app/page.js
@@ -89,7 +89,10 @@ const HomePage = () => {
       </h1>
 
       {islandId ? (
-        <button className="bg-white" onClick={() => router.push(`/island/${islandId}`)}>
+        <button
+          className="bg-gray-800 text-[#fff8e1] font-bold p-4 rounded-xl hover:bg-[#fff8e1] hover:text-gray-800 transition w-full sm:w-96 active:scale-95"
+          onClick={() => router.push(`/island/${islandId}`)}
+        >
           我的島嶼
         </button>
       ) : (
